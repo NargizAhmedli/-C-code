ab94b5c772d31939e898b7106019c1cf5b920c08
NargizAhmedli <99806064+NargizAhmedli@users.noreply.github.com>
2022-03-17 10:56:40 +0400

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Group group = new Group();
+            group.no = "AP203";
+            Console.WriteLine(group.No);
+        }
+    }
+}
