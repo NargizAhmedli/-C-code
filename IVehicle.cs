bc3cc61100dfcab11282d23628a4a5ff704d7d19
NargizAhmedli <99806064+NargizAhmedli@users.noreply.github.com>
2022-03-22 23:30:59 +0400

    Create IVehicle.cs

---
+++ IVehicle.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp14
+{
+    internal interface IVehicle
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+    }
+}
