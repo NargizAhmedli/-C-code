3ab97414e53fc77edca4d507beb5af1a25aae21b
NargizAhmedli <99806064+NargizAhmedli@users.noreply.github.com>
2022-03-22 23:32:15 +0400

    Create IDrive.cs

---
+++ IDrive.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp14
+{
+    internal interface IDrive
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+    }
+}
