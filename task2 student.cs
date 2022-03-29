2fa2f77748e0342adb834e9599bd0fa170e86281
NargizAhmedli <99806064+NargizAhmedli@users.noreply.github.com>
2022-03-29 15:44:13 +0400

    Create task2 student.cs

---
+++ task2 student.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp18
+{
+    internal class Student
+    {
+        public string FullName { get; set; }    
+        public double GroupNo { get; set; } 
+        public int Age { get; set; }    
+    }
+}
