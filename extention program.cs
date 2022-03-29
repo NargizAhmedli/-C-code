2e829d2d51e1c5a21c06d55b1185b36ea80787c1
NargizAhmedli <99806064+NargizAhmedli@users.noreply.github.com>
2022-03-29 15:40:21 +0400

    Create extention program.cs

---
+++ extention program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace _28032022_task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 5;
+            Console.WriteLine(number.isodd));
+            Console.WriteLine(number.iseven)); 
+            string word = "nergis7";
+            Console.WriteLine(word.IsContainsDigit));
+
+            Console.WriteLine(word.ToCapitalize));
+            Console.WriteLine(word.GetValueIndexes));
+        }
+    }
+}
